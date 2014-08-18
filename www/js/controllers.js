0eb21ccf460255fe218d1573930550289fb57ae9
Jibidus <jibidus@gmail.com>
2014-08-18 08:49:52 +0200

    Show only visible slide, next and previous one

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,15 +5,8 @@ angular.module('kaamelott.controllers', [])
 	// This means only 5 slides max (2 previous, 2 next, and the one is shown) will be in the DOM. 
 	var bufferedSlides = 2;
 	
-	$scope.sentences = [];
-	$scope.sentences.push(Sentences.get(0));
-	repeat(10, function() {
-		var lastSentence = $scope.sentences[$scope.sentences.length-1];
-		var nextSentence = Sentences.next(lastSentence);
-		$scope.sentences.push(nextSentence);
-	});
+	$scope.sentences = Sentences.all();
 	$scope.currentSlide = 0;
-//	$ionicSlideBoxDelegate.update();
 	
 	$scope.next = function() {
 		$ionicSlideBoxDelegate.next();
@@ -23,86 +16,8 @@ angular.module('kaamelott.controllers', [])
 	};
 	
 	$scope.onSlideChanged = function(newSlideIndex) {
-		console.log("----------- after slide changed");
-	   console.log("Gone to: " + (newSlideIndex+1) + " / " + $scope.sentences.length);
 	   $scope.currentSlide = $ionicSlideBoxDelegate.currentIndex();
-	   console.log("$scope.currentSlide=" + $scope.currentSlide);
-
-	   var nbPreviousSlidesOutOfBuffer = newSlideIndex+1 - bufferedSlides - 1;
-	   var nbNextSlidesOutOfBuffer = ($scope.sentences.length - (newSlideIndex+1)) - bufferedSlides;
-
-	   console.log("Remove " + nbPreviousSlidesOutOfBuffer + " previous slide(s)...");
-	   removePreviousSlides(nbPreviousSlidesOutOfBuffer);
-	   
-	   //console.log("Remove " + nbNextSlidesOutOfBuffer + " next slide(s)...");
-	   //removeNextSlides(nbNextSlidesOutOfBuffer);
-
-	   console.log("Now: " + ($ionicSlideBoxDelegate.currentIndex()+1) + " / " + $scope.sentences.length);
-	   if (nbPreviousSlidesOutOfBuffer != 0) {
-		   console.log("update()");
-		   $ionicSlideBoxDelegate.update();
-	   }
-	   console.log("$scope.currentSlide=" + $scope.currentSlide);
    };
-   
-   function removePreviousSlides(nbSlides){
-	   var jumpSlide = 0;
-	   if (nbSlides > 0) {
-		   // FIXME Use slice to avoid loop?
-		   repeat(nbSlides, function() {
-			   $scope.sentences.shift();
-			   //$scope.slideIndex--;
-			   jumpSlide--;
-		   });
-	   } else {
-		   repeat(-nbSlides, function() {
-			   var firstSentence = $scope.sentences[0];
-			   var previousSentence = Sentences.previous(firstSentence);
-			   if (previousSentence != null) {
-				   $scope.sentences.unshift(previousSentence);
-				   jumpSlide++;
-			   } else {
-				   // TODO Manage loop in service
-				   console.log("Cannot add previous slide: first slide reached");
-			   }
-		   });
-	   }
-	   if (jumpSlide != 0){
-		   $scope.currentSlide = $scope.currentSlide + jumpSlide;
-		 //  $ionicSlideBoxDelegate.slide($ionicSlideBoxDelegate.currentIndex() + jumpSlide, 0);
-	   }
-   }
-   
-   function removeNextSlides(nbSlides){
-	   if (nbSlides > 0) {
-		   repeat(nbSlides, function() {
-			   $scope.sentences.pop();
-		   });
-	   } else {
-		   repeat(-nbSlides, function() {
-			   var lastSentence = $scope.sentences[$scope.sentences.length-1];
-			   var nextSentence = Sentences.next(lastSentence);
-			   if (nextSentence != null) {
-				   $scope.sentences.push(nextSentence);
-			   } else {
-				   // TODO manage loop (in service)
-//				   if (currentSentenceIndex >= Sentences.size()) {
-//				   Sentences.shuffle();
-//				   currentSlideIndex = -1;
-				   // TODO will raise duplicate when inserting into sentences
-//			   }
-			   }
-		   });
-	   }
-   }
-   
-   function repeat(times, callback) {
-	   var i = 1;
-	   while(i <= times) {
-		   callback();
-		   i++;
-	   }
-   }
    
    $scope.updateRate = function() {
 	   console.debug("new rate: ");
